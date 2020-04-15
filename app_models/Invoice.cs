a731fed01a5b2278a0d81a98bfd6ea3a28ea0984
BuildTools <unconfigured@null.spigotmc.org>
2020-04-15 11:26:36 -0400

    3.2 Modèles

---
+++ app_models/Invoice.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace app_models
+{
+    public class Invoice : INotifyPropertyChanged
+    {
+
+        public int InvoiceId { get; private set; }
+        public readonly DateTime CreationDateTime;
+        public Customer customer;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public double SubTotal { get; set; }
+        public double FedTax {
+
+            get { return SubTotal * 0.05; }
+            set { }
+
+        }
+        public double ProvTax {
+
+            get { return SubTotal * 0.09975; }
+            set { }
+
+        }
+        public double Total {
+
+            get { return SubTotal + FedTax + ProvTax; }
+            set { }
+
+        }
+
+        public Invoice()
+        {
+
+            CreationDateTime = DateTime.Now;
+
+        }
+
+        public Invoice(Customer c)
+        {
+
+            customer = c;
+            CreationDateTime = DateTime.Now;
+
+        }
+
+
+    }
+}
